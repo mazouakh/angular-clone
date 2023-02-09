5e08b705cbf5f3324c972f5bd81a5975645f04a6
Abderrahmane Zouakh <zzmoubarek@gmail.com>
2023-02-09 18:52:08 +0100

    CreditCard directive

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,27 @@
+// Declaring new directives
+class CreditCardDirective {
+	constructor(public element: HTMLElement) {}
+
+	formatCreditCardNumber(element: HTMLInputElement) {
+		// replace anything that is not a number with an empty string.
+		let formatedNumber = element.value.replace(/[^\d]/g, "");
+		// limit the length of the phone number to 10 characters.
+		formatedNumber = formatedNumber.length > 10 ? formatedNumber.slice(0, 16) : formatedNumber;
+		// put a space between two numbers
+		const numbersGroups: string[] = [];
+		for (let i = 0; i < formatedNumber.length; i += 4) {
+			numbersGroups.push(formatedNumber.slice(i, i + 4));
+		}
+		element.value = numbersGroups.join(" ");
+	}
+
+	init() {
+		this.element.addEventListener("input", (event) => {
+			this.formatCreditCardNumber(event.target as HTMLInputElement);
+		});
+	}
+}
+
 class PhoneNumberDirective {
 	constructor(public element: HTMLElement) {}
 
@@ -26,12 +50,25 @@ class PhoneNumberDirective {
 	}
 }
 
+// FRAMEWORK : put the directives on the html elements whit the corresponding attributes
+
 // select all HTML elements on the page that have the attribute phone-number
-const elements = document.querySelectorAll<HTMLElement>("[phone-number]");
-if (elements.length > 0) {
-	elements.forEach((element) => {
+const phoneNumberElements = document.querySelectorAll<HTMLElement>("[phone-number]");
+if (phoneNumberElements.length > 0) {
+	phoneNumberElements.forEach((element) => {
 		// create a new instance of the directive and pass the HTML element as a parameter
 		const directive = new PhoneNumberDirective(element);
+		// then call the init function
+		directive.init();
+	});
+}
+
+// select all HTML elements on the page that have the attribute credit-card
+const creditCardElements = document.querySelectorAll<HTMLElement>("[credit-card]");
+if (creditCardElements.length > 0) {
+	creditCardElements.forEach((element) => {
+		// create a new instance of the directive and pass the HTML element as a parameter
+		const directive = new CreditCardDirective(element);
 		// then call the init function
 		directive.init();
 	});
