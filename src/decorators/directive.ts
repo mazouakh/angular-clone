91c3398bf0db13fc43263d00f916cdeaea428ba9
Abderrahmane Zouakh <zzmoubarek@gmail.com>
2023-02-13 23:58:57 +0100

    Introducing decorators . Created the @Directive() decorator that will be used in each directive class in order to specify it's selector and providers.

--- src/decorators/directive.ts
+++ src/decorators/directive.ts
@@ -1,11 +1,31 @@
 import { Directive } from "../framework/types";
 
+/**
+ * A decorator that marks a class as a directive and modifies it before returning it.
+ *
+ * @param Metadata The selector and providers? of the directive.
+ * @returns A decorated class containing the selector and providers as static properties.
+ */
 export function Directive(Metadata: Directive) {
+	/**
+	 * A decorator is a function that needs to return a function
+	 * the retuned function takes as a parameter the original class the decorator is applied to
+	 */
 	return function (decoratedClass) {
+		/**
+		 * The decorated class needs to have a static property called "selector"
+		 * which is the selector of the directive.
+		 */
 		decoratedClass["selector"] = Metadata.selector;
+		/*
+		 * The decorated class needs to have a static property called "providers"
+		 * which is the providers of the directive if available or an empty array.
+		 */
 		decoratedClass["providers"] = Metadata.providers || [];
-		console.log(decoratedClass);
 
+		/**
+		 * returns the decorated class
+		 */
 		return decoratedClass;
 	};
 }
